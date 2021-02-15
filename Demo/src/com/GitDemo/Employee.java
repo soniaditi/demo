da9fe575c15a4bb2bea0b46a14de3000140c5b02
aditi <soniaditi2298@gmail.com>
2021-02-15 15:36:49 +0530

    added getter and setter

--- Demo/src/com/GitDemo/Employee.java
+++ Demo/src/com/GitDemo/Employee.java
@@ -3,4 +3,17 @@ package com.GitDemo;
 public class Employee {
 	int empId;
 	float salary;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public float getSalary() {
+		return salary;
+	}
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+	
 }
